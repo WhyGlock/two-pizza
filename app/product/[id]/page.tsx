721c9e20d083efec6340c2d1000aec6c42ff4ed0
WhyGlock <cydaabeatz@gmail.com>
2025-04-15 15:57:09 +0300

    найс
    7:53:30

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,62 @@
+
+import { ProductImage, Title } from '@/components/shared';
+import { Container } from '@/components/shared/container';
+import { GroupVariants } from '@/components/shared/group-variants';
+import { prisma } from '@/prisma/prisma-client';
+import { notFound } from 'next/navigation';
+
+export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
+  const product = await prisma.product.findFirst({
+    where: { id: Number(id) },
+    include: {
+      ingredients: true,
+      category: {
+        include: {
+          products: {
+            include: {
+              items: true,
+            },
+          },
+        },
+      },
+      items: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+        <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} className="" size={40}/>
+
+            <div className="w-[490px] bg-[#e9e9e9] p-7">
+        <Title text={product.name} size="md" className="font-extrabold mb-1"/>
+
+        <p className="text-gray-400">я бы скушал, но у меня нет ротика</p>
+
+        <GroupVariants 
+        selectedValue="2"
+        items={[
+            {
+            name: 'Маленькая',
+            value: '1',
+            },
+            {
+            name: 'Средняя',
+            value: '2',
+            },
+            {
+                name: 'Большая',
+                value: '3',
+                disabled: true,
+            },
+        
+        ]}  />
+            </div>
+        </div>
+    </Container>
+  );
+}
