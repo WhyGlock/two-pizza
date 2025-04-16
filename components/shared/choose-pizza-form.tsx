c85a61c73ef5d3aa18edab0451f6ca30b652bf88
WhyGlock <cydaabeatz@gmail.com>
2025-04-16 15:30:34 +0300

    продукты без круга, пиццы с ним
    9 01 30

--- components/shared/choose-pizza-form.tsx
+++ components/shared/choose-pizza-form.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import React from "react";
-
+import { ProductImage } from "./product-image";
 import { Title } from "./title";
 import { Button } from "../ui/button";
 
@@ -8,25 +8,25 @@ interface Props {
     imageUrl: string;
     name: string;
     className?: string;
+    ingredients: any[];
+    items?: any[];
     onClickAdd?: VoidFunction;
 }
 
-export const ChooseProductForm: React.FC<Props> = ({ 
+export const ChoosePizzaForm: React.FC<Props> = ({ 
     name,
+    items,
     imageUrl,
+    ingredients,
     onClickAdd,
     className, 
 }) => {
     const textDetails = '30 см, традиционное тесто 30';
     const totalPrice = 450;
+    const size = 30;
     return ( <div className={cn(className, 'flex flex-1')}>
-            <div className="flex items-center justify-center flex-1 relative  w-full">
-            <img 
-            src={imageUrl}
-            alt={name}
-            className="relative left-2 top-2 translition-all z-10 duration-300 w-[350px] h-[350px]"
-            />            
-            </div>
+                      <ProductImage imageUrl={imageUrl} size={size} />
+            
             <div className="w-[490px] bg-[#f7f6f5] p-7">
                 <Title text={name} size="md" className="font-extrabold mb-1"/>
 
--- components/shared/modals/choose-product-modal.tsx
+++ components/shared/modals/choose-product-modal.tsx
@@ -7,20 +7,27 @@ import { Title } from '@radix-ui/react-dialog';
 import { useRouter } from 'next/navigation';
 import React  from 'react'
 import { ChooseProductForm } from '../choose-product-form';
+import { ProductWithRelations } from '@/@types/prisma';
+import { ChoosePizzaForm } from '../choose-pizza-form';
 
 interface Props {
-    product: Product;
+    product: ProductWithRelations;
     className?: string;
 }
 
 export const ChooseProductModal: React.FC<Props> = ({product, className }) => {
     const router = useRouter();
+    const izPizzaForm = Boolean(product.items[0].pizzaType);
 
     return (
         <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
             <DialogContent className={cn("p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden", className)}>
-          <ChooseProductForm imageUrl={product.imageUrl} name={product.name} ingredients={[]} />
-            </DialogContent>
+                {izPizzaForm ? (
+                    <ChoosePizzaForm imageUrl={product.imageUrl} name={product.name} ingredients={[]}/>
+                ): ( 
+                <ChooseProductForm imageUrl={product.imageUrl} name={product.name} />
+                )}
+            </DialogContent>    
         </Dialog>
     );
 }
